5beee8f5ed4b1a1093f03ff1e1cf6b3602c9e00a
jugurthales <ines.benalia21@gmail.com>
2026-06-10 14:53:36 +0200

    correction du script : mauvaise réference de code (06_ws2812 au lieu de 06_spi_ws2812

--- taches/Tache2.py
+++ taches/Tache2.py
@@ -1,106 +1,287 @@
+import spidev
+import threading  
+import numpy
+from numpy import sin, cos, pi
 import time
-from rpi_ws281x import *
-import argparse
+class Adeept_SPI_LedPixel(threading.Thread):
 
-# Configuration de la bande LED :
-LED_COUNT      = 14      # Nombre de pixels LED.
-LED_PIN        = 12      # Broche GPIO reliée aux pixels (12 utilise le PWM matériel).
-#LED_PIN        = 10     # Broche GPIO reliée aux pixels (10 utilise le SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # Fréquence du signal LED en hertz (généralement 800 kHz).
-LED_DMA        = 10      # Canal DMA utilisé pour générer le signal (essayer 10).
-LED_BRIGHTNESS = 100     # 0 = le plus sombre, 255 = le plus lumineux.
-LED_INVERT     = False   # True pour inverser le signal (adaptation de niveau par transistor NPN).
-LED_CHANNEL    = 0       # Mettre à 1 pour les GPIO 13, 19, 41, 45 ou 53.
-colors = {
+    colors = {
     "R": [255, 0, 0],
     "G": [0, 255, 0],
     "B": [0, 0, 255],
     "N": [0, 0, 0],
-} # Dictionnaire de couleurs pour les LED (R, G, B, N pour éteint).
+    }
 
-def setup():
-  global strip
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-  args = parser.parse_args()
+    def __init__(self, count = 14, bright = 255, sequence='GRB', bus = 0, device = 0, *args, **kwargs):
+        self.set_led_type(sequence)
+        self.set_led_count(count)
+        self.set_led_brightness(bright)
+        self.led_begin(bus, device)
+        self.lightMode = 'none'
+        self.colorBreathR = 0
+        self.colorBreathG = 0
+        self.colorBreathB = 0
+        self.breathSteps = 10
+        #self.spi_gpio_info()
+        self.set_all_led_color(0,0,0)
+        super(Adeept_SPI_LedPixel, self).__init__(*args, **kwargs)
+        self.__flag = threading.Event()
+        self.__flag.clear()
+    def led_begin(self, bus = 0, device = 0):
+        self.bus = bus
+        self.device = device
+        try:
+            self.spi = spidev.SpiDev()
+            self.spi.open(self.bus, self.device)
+            self.spi.mode = 0
+            self.led_init_state = 1
+        except OSError:
+            print("Please check the configuration in /boot/firmware/config.txt.")
+            if self.bus == 0:
+                print("You can turn on the 'SPI' in 'Interface Options' by using 'sudo raspi-config'.")
+                print("Or make sure that 'dtparam=spi=on' is not commented, then reboot the Raspberry Pi. Otherwise spi0 will not be available.")
+            else:
+                print("Please add 'dtoverlay=spi{}-2cs' at the bottom of the /boot/firmware/config.txt, then reboot the Raspberry Pi. otherwise spi{} will not be available.".format(self.bus, self.bus))
+            self.led_init_state = 0
+            
+    def check_spi_state(self):
+        return self.led_init_state
+        
+    def spi_gpio_info(self):
+        if self.bus == 0:
+            print("SPI0-MOSI: GPIO10(WS2812-PIN)  SPI0-MISO: GPIO9  SPI0-SCLK: GPIO11  SPI0-CE0: GPIO8  SPI0-CE1: GPIO7")
+        elif self.bus == 1:
+            print("SPI1-MOSI: GPIO20(WS2812-PIN)   SPI1-MISO: GPIO19  SPI1-SCLK: GPIO21  SPI1-CE0: GPIO18  SPI1-CE1: GPIO17  SPI0-CE1: GPIO16")
+        elif self.bus == 2:
+            print("SPI2-MOSI: GPIO41(WS2812-PIN)   SPI2-MISO: GPIO40  SPI2-SCLK: GPIO42  SPI2-CE0: GPIO43  SPI2-CE1: GPIO44  SPI2-CE1: GPIO45")
+        elif self.bus == 3:
+            print("SPI3-MOSI: GPIO2(WS2812-PIN)  SPI3-MISO: GPIO1  SPI3-SCLK: GPIO3  SPI3-CE0: GPIO0  SPI3-CE1: GPIO24")
+        elif self.bus == 4:
+            print("SPI4-MOSI: GPIO6(WS2812-PIN)  SPI4-MISO: GPIO5  SPI4-SCLK: GPIO7  SPI4-CE0: GPIO4  SPI4-CE1: GPIO25")
+        elif self.bus == 5:
+            print("SPI5-MOSI: GPIO14(WS2812-PIN)  SPI5-MISO: GPIO13  SPI5-SCLK: GPIO15  SPI5-CE0: GPIO12  SPI5-CE1: GPIO26")
+        elif self.bus == 6:
+            print("SPI6-MOSI: GPIO20(WS2812-PIN)  SPI6-MISO: GPIO19  SPI6-SCLK: GPIO21  SPI6-CE0: GPIO18  SPI6-CE1: GPIO27")
+    
+    def led_close(self):
+        self.set_all_led_rgb([0,0,0])
+        self.spi.close()
+    
+    def set_led_count(self, count):
+        self.led_count = count
+        self.led_color = [0,0,0] * self.led_count
+        self.led_original_color = [0,0,0] * self.led_count
+    
+    def set_led_type(self, rgb_type):
+        try:
+            led_type = ['RGB','RBG','GRB','GBR','BRG','BGR']
+            led_type_offset = [0x06,0x09,0x12,0x21,0x18,0x24]
+            index = led_type.index(rgb_type)
+            self.led_red_offset = (led_type_offset[index]>>4) & 0x03
+            self.led_green_offset = (led_type_offset[index]>>2) & 0x03
+            self.led_blue_offset = (led_type_offset[index]>>0) & 0x03
+            return index
+        except ValueError:
+            self.led_red_offset = 1
+            self.led_green_offset = 0
+            self.led_blue_offset = 2
+            return -1
+    
+    def set_led_brightness(self, brightness):
+        self.led_brightness = brightness
+        for i in range(self.led_count):
+            self.set_led_rgb_data(i, self.led_original_color)
+            
+    def set_ledpixel(self, index, r, g, b):
+        p = [0,0,0]
+        p[self.led_red_offset] = round(r * self.led_brightness / 255)
+        p[self.led_green_offset] = round(g * self.led_brightness / 255)
+        p[self.led_blue_offset] = round(b * self.led_brightness / 255)
+        self.led_original_color[index*3+self.led_red_offset] = r
+        self.led_original_color[index*3+self.led_green_offset] = g
+        self.led_original_color[index*3+self.led_blue_offset] = b
+        for i in range(3):
+            self.led_color[index*3+i] = p[i]
 
-  # Création de l'objet NeoPixel avec la configuration appropriée.
-  strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-  # Initialisation de la bibliothèque (à appeler une seule fois avant toute autre fonction).
-  strip.begin()
-  # strip.setBrightness(10) # 0~255, règle la luminosité de la LED RVB WS2812.
+    def set_led_color_data(self, index, r, g, b):
+        self.set_ledpixel(index, r, g, b)  
+        
+    def set_led_rgb_data(self, index, color):
+        self.set_ledpixel(index, color[0], color[1], color[2])   
+        
+    def set_led_color(self, index, r, g, b):
+        self.set_ledpixel(index, r, g, b)
+        self.show() 
+        
+    def set_led_rgb(self, index, color):
+        self.set_led_rgb_data(index, color)   
+        self.show() 
+    
+    def set_all_led_color_data(self, r, g, b):
+        for i in range(self.led_count):
+            self.set_led_color_data(i, r, g, b)
+            
+    def set_all_led_rgb_data(self, color):
+        for i in range(self.led_count):
+            self.set_led_rgb_data(i, color)   
+        
+    def set_all_led_color(self, r, g, b):
+        for i in range(self.led_count):
+            self.set_led_color_data(i, r, g, b)
+        self.show()
+        
+    def set_all_led_rgb(self, color):
+        for i in range(self.led_count):
+            self.set_led_rgb_data(i, color) 
+        self.show()
+    
+    def write_ws2812_numpy8(self):
+        d = numpy.array(self.led_color).ravel()        #Converts data into a one-dimensional array
+        tx = numpy.zeros(len(d)*8, dtype=numpy.uint8)  #Each RGB color has 8 bits, each represented by a uint8 type data
+        for ibit in range(8):                          #Convert each bit of data to the data that the spi will send
+            tx[7-ibit::8]=((d>>ibit)&1)*0x78 + 0x80    #T0H=1,T0L=7, T1H=5,T1L=3   #0b11111000 mean T1(0.78125us), 0b10000000 mean T0(0.15625us)  
+        if self.led_init_state != 0:
+            if self.bus == 0:
+                self.spi.xfer(tx.tolist(), int(8/1.25e-6))         #Send color data at a frequency of 6.4Mhz
+            else:
+                self.spi.xfer(tx.tolist(), int(8/1.0e-6))          #Send color data at a frequency of 8Mhz
+        
+    def write_ws2812_numpy4(self):
+        d=numpy.array(self.led_color).ravel()
+        tx=numpy.zeros(len(d)*4, dtype=numpy.uint8)
+        for ibit in range(4):
+            tx[3-ibit::4]=((d>>(2*ibit+1))&1)*0x60 + ((d>>(2*ibit+0))&1)*0x06 + 0x88  
+        if self.led_init_state != 0:
+            if self.bus == 0:
+                self.spi.xfer(tx.tolist(), int(4/1.25e-6))         
+            else:
+                self.spi.xfer(tx.tolist(), int(4/1.0e-6))       
+        
+    def show(self, mode = 1):
+        if mode == 1:
+            write_ws2812 = self.write_ws2812_numpy8
+        else:
+            write_ws2812 = self.write_ws2812_numpy4
+        write_ws2812()
+        
+    def wheel(self, pos):
+        if pos < 85:
+            return [(255 - pos * 3), (pos * 3), 0]
+        elif pos < 170:
+            pos = pos - 85
+            return [0, (255 - pos * 3), (pos * 3)]
+        else:
+            pos = pos - 170
+            return [(pos * 3), 0, (255 - pos * 3)]
+    
+    def hsv2rgb(self, h, s, v):
+        h = h % 360
+        rgb_max = round(v * 2.55)
+        rgb_min = round(rgb_max * (100 - s) / 100)
+        i = round(h / 60)
+        diff = round(h % 60)
+        rgb_adj = round((rgb_max - rgb_min) * diff / 60)
+        if i == 0:
+            r = rgb_max
+            g = rgb_min + rgb_adj
+            b = rgb_min
+        elif i == 1:
+            r = rgb_max - rgb_adj
+            g = rgb_max
+            b = rgb_min
+        elif i == 2:
+            r = rgb_min
+            g = rgb_max
+            b = rgb_min + rgb_adj
+        elif i == 3:
+            r = rgb_min
+            g = rgb_max - rgb_adj
+            b = rgb_max
+        elif i == 4:
+            r = rgb_min + rgb_adj
+            g = rgb_min
+            b = rgb_max
+        else:
+            r = rgb_max
+            g = rgb_min
+            b = rgb_max - rgb_adj
+        return [r, g, b]
+    
+    def police(self):
+        self.lightMode = 'police'
+        self.resume()
+        
+    def breath(self, R_input, G_input, B_input):
+        self.lightMode = 'breath'
+        self.colorBreathR = R_input
+        self.colorBreathG = G_input
+        self.colorBreathB = B_input
+        self.resume()    
+            
+    def resume(self):
+        self.__flag.set()
 
-"""
 
-# Définition des fonctions qui animent les LED de différentes manières.
+    def breathProcessing(self):
+        while self.lightMode == 'breath':
+            for i in range(0,self.breathSteps):
+                if self.lightMode != 'breath':
+                    break
+                self.set_all_led_color(self.colorBreathR*i/self.breathSteps, self.colorBreathG*i/self.breathSteps, self.colorBreathB*i/self.breathSteps)
+                #self.show()
+                time.sleep(0.03)
+            for i in range(0,self.breathSteps):
+                if self.lightMode != 'breath':
+                    break
+                self.set_all_led_color(self.colorBreathR-(self.colorBreathR*i/self.breathSteps), self.colorBreathG-(self.colorBreathG*i/self.breathSteps), self.colorBreathB-(self.colorBreathB*i/self.breathSteps))
+                #self.show()
+                time.sleep(0.03)
+    def policeProcessing(self):
+        while self.lightMode == 'police':
+            for i in range(0,3):
+                self.set_all_led_color_data(0,0,255)
+                self.show()
+                time.sleep(0.05)
+                self.set_all_led_color_data(0,0,0)
+                self.show()
+                time.sleep(0.05)
+            if self.lightMode != 'police':
+                break
+            time.sleep(0.1)
+            for i in range(0,3):
+                self.set_all_led_color_data(255,0,0)
+                self.show()
+                time.sleep(0.05)
+                self.set_all_led_color_data(0,0,0)
+                self.show()
+                time.sleep(0.05)
+            time.sleep(0.1)
+            
+            
+    def lightChange(self):
+        if self.lightMode == 'none':
+            self.pause()
+        elif self.lightMode == 'police':
+            self.policeProcessing()
+        elif self.lightMode == 'breath':
+            self.breathProcessing()    
+    
+    def run(self):
+        while 1:
+            self.__flag.wait()
+            self.lightChange()
+            pass
+        
+       
 
-Arguments:
-R, G, B – les valeurs de couleur rouge, verte et bleue (0-255) pour la fonction colorWipe.
+    def set_one_led(self, choosenLed, choosenColor, brightness=255):
+        if 1 <= choosenLed <= 14 and choosenColor in self.colors:
+            scaled_colors = [round(c * brightness / 255) for c in self.colors.get(choosenColor)]
+            self.set_led_rgb(choosenLed - 1, scaled_colors)
+            
+        else:
+            print("\033[1;31m Invalid input. Please enter a number between 1 and 14 for the LED and a color (R, G, B, N).\033[0m")
 
-"""
-def colorWipe( R, G, B):
-    """Balaye une couleur sur tout l'affichage, un pixel à la fois."""
-    color = Color(R,G,B)
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-
-
-"""
-
-Fonction pour allumer une LED spécifique avec une couleur et une luminosité données.
-
-Arguments:
-choosenLed – le numéro de la LED à allumer (1-14).
-choosenColor – la couleur à afficher (R, G, B, N pour éteint).
-brightness – la luminosité de la LED (0-255, par défaut 255 pour la luminosité maximale).
-
-"""
-def run(choosenLed, choosenColor, brightness=255):
-    if 1 <= choosenLed <= 14 and choosenColor in colors:
-        scaled_colors = [round(c * brightness / 255) for c in colors.get(choosenColor)]
-        strip.setPixelColor(choosenLed -1, Color(*scaled_colors)) 
-        strip.show() 
-    else:
-        print("\033[1;31m Invalid input. Please enter a number between 1 and 14 for the LED and a color (R, G, B, N).\033[0m")
-
-"""
-Fonction pour vérifier le modèle de Raspberry Pi et afficher un message d'avertissement si le modèle 5 est détecté,
-car la bande LED WS2812 n'est pas officiellement prise en charge sur ce modèle.
-
-returns:
-3, 4, 5 ou None selon le modèle de Raspberry Pi détecté.
-
-"""
-def check_rpi_model():
-    _, result = run_command("cat /proc/device-tree/model |awk '{print $3}'")
-    result = result.strip()
-    if result == '3':
-        return 3
-    elif result == '4':
-        return 4
-    elif result == '5':
-        return 5
-    else:
-        return None
-
-"""
-Fonction pour exécuter une commande shell et retourner son statut et sa sortie.
-Arguments:
-cmd – la commande shell à exécuter.
-
-Retourne:
-status – le code de retour de la commande (0 pour succès, autre pour erreur).
-result – la sortie de la commande en tant que chaîne de caractères.
-
-"""
-def run_command(cmd=""):
-    import subprocess
-    p = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    result = p.stdout.read().decode('utf-8')
-    status = p.poll()
-    return status, result
 
 """
 
@@ -111,35 +292,31 @@ modèle.
 
 """
 if __name__ == '__main__':
+    leds = Adeept_SPI_LedPixel(14, 255)   
     try:
-      value = 1
-      rpi_model = check_rpi_model()
-      if rpi_model == 5:
-        print("\033[1;33m WS2812 officially does not support Raspberry Pi 5 for the time being, and the WS2812 LED cannot be used on Raspberry Pi 5.\033[0m")
-        value = 0
-      else:
-        setup()
-      while value!= 0:
-       
-        print("\033[1;34m Please enter the LED number (1-14) ")
-        choosenLed = input()
-        if not str(choosenLed).isdigit(): 
-            print("\033[1;31m Invalid LED number. Please enter a number between 1 and 14.\033[0m")
-            continue
-        elif int(choosenLed) < 1 or int(choosenLed) > 14:
-            print("\033[1;31m Invalid LED number. Please enter a number between 1 and 14.\033[0m")
-            continue
         
-        print("\033[1;34m Please enter the color (R, G, B, N) ")
-        choosenColor = input().upper()
-        print("\033[1;34m Please enter the brightness (0-255) ")
-        choosenBrightness = input()
-        if not str(choosenBrightness).isdigit(): 
-            print("\033[1;31m Invalid brightness. Please enter a number between 0 and 255.\033[0m")
-            continue
-        elif int(choosenBrightness) < 0 or int(choosenBrightness) > 255:
-            print("\033[1;31m Invalid brightness. Please enter a number between 0 and 255.\033[0m")
-            continue
-        run(int(choosenLed), choosenColor, int(choosenBrightness))
+        value = 1
+        while value!= 0:
+        
+            print("\033[1;34m Please enter the LED number (1-14) ")
+            choosenLed = input()
+            if not str(choosenLed).isdigit(): 
+                print("\033[1;31m Invalid LED number. Please enter a number between 1 and 14.\033[0m")
+                continue
+            elif int(choosenLed) < 1 or int(choosenLed) > 14:
+                print("\033[1;31m Invalid LED number. Please enter a number between 1 and 14.\033[0m")
+                continue
+            
+            print("\033[1;34m Please enter the color (R, G, B, N) ")
+            choosenColor = input().upper()
+            print("\033[1;34m Please enter the brightness (0-255) ")
+            choosenBrightness = input()
+            if not str(choosenBrightness).isdigit(): 
+                print("\033[1;31m Invalid brightness. Please enter a number between 0 and 255.\033[0m")
+                continue
+            elif int(choosenBrightness) < 0 or int(choosenBrightness) > 255:
+                print("\033[1;31m Invalid brightness. Please enter a number between 0 and 255.\033[0m")
+                continue
+            leds.set_one_led(int(choosenLed), choosenColor, int(choosenBrightness))
     except KeyboardInterrupt:
-      colorWipe(0, 0, 0)
+        leds.led_close()
