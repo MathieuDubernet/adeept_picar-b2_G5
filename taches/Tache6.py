fcaf4c5cf6d15b9ad0dc60043cab9224562e94e2
jugurthales <ines.benalia21@gmail.com>
2026-06-10 22:10:57 +0200

    Tache 6: transformation en objet

--- taches/Tache6.py
+++ taches/Tache6.py
@@ -2,36 +2,34 @@
 import time
 from gpiozero import InputDevice
 
-# Définition des broches GPIO pour les capteurs IR réflectifs
-line_pin_left = 22
-line_pin_middle = 27
-line_pin_right = 17
+class Adeept_infrared:
+    # Définition des broches GPIO pour les capteurs IR réflectifs
+    line_pin_left = 22
+    line_pin_middle = 27
+    line_pin_right = 17
 
-# Initialisation des capteurs IR réflectifs
-left = InputDevice(pin=line_pin_left)
-middle = InputDevice(pin=line_pin_middle)
-right = InputDevice(pin=line_pin_right)
+    # Initialisation des capteurs IR réflectifs
+    def __init__(self):
+        self.left = InputDevice(pin=self.line_pin_left)
+        self.middle = InputDevice(pin=self.line_pin_middle)
+        self.right = InputDevice(pin=self.line_pin_right)
 
-"""
-Fonction permettant de déterminer l'état des trois capteurs IR réflectifs (gauche, milieu, droite) et de retourner ces états sous forme de liste.
-Returns:
-Une liste contenant les états des capteurs dans l'ordre [gauche, milieu, droite], où chaque état est soit 0 (capteur non activé) soit 1 (capteur activé).
-
-"""
-
-def run():
-    status_right = right.value
-    status_middle = middle.value
-    status_left = left.value
-    return [status_left, status_middle, status_right]
+    """
+    Fonction permettant de déterminer l'état des trois capteurs IR réflectifs (gauche, milieu, droite) et de retourner ces états sous forme de liste.
+    """
+    def read(self):
+        """Retourne [gauche, milieu, droite] sous forme de booléens."""
+        status_right = self.right.value
+        status_middle = self.middle.value
+        status_left = self.left.value
+        return [status_left, status_middle, status_right]
 
 # Exécution de la fonction run pour afficher les états des capteurs IR réflectifs
 if __name__ == '__main__':
+    infrared = Adeept_infrared()
     try:
       while 1:
-        print(run())
+        print(infrared.run())
         time.sleep(0.3)
     except KeyboardInterrupt:
         pass
-
-
